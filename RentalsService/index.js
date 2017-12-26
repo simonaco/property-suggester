37c850f4fa6796c1ac2637699e53b57700960c0a
simonaco-circle <simona_cotin@yahoo.com>
2017-12-26 09:28:30 +0000

    initial commit

---
+++ RentalsService/index.js
@@ -0,0 +1,25 @@
+const request = require('request');
+module.exports = function(context, req) {
+  context.log('Function execution start');
+  const body = req.body;
+  const options = {
+    url: 'https://api.zoopla.co.uk/api/v1/property_listings.js',
+    qs: {
+      api_key: process.env.API_KEY,
+      area: body.area,
+      listing_status: body.listing_status,
+      order_by: body.order_by
+    },
+    method: 'GET'
+  };
+  request(options, (err, result) => {
+    if (err) {
+      context.log('Error' + err);
+      context.done();
+      return;
+    } else {
+      context.log(result.body);
+      context.done();
+    }
+  });
+};
